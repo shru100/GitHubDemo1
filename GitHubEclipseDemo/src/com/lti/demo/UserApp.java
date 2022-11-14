9cdeb1148758c7f9c9ce195e8799bbda426d83fa
shru100 <srushpunyarthi@gmail.com>
2022-11-14 12:21:33 +0530

    First Commit

---
+++ GitHubEclipseDemo/src/com/lti/demo/UserApp.java
@@ -0,0 +1,11 @@
+package com.lti.demo;
+
+public class UserApp {
+
+	public static void main(String[]args) {
+		Employee e = new Employee(100, "Jay", 50000);
+		System.out.println(e);
+		System.out.println();
+		
+	}
+}
